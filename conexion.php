30466b3ef5fa72e00e4098d7998984f513ce53ca
FISEP09 <FISEP09>
2017-02-25 18:24:39 -0600

    rge

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+/*$servidor="localhost";
+$usuario="root";
+$pwd="alex";
+$BD="servcios_libres";*/
+
+$servidor="mysql.hostinger.mx";
+$usuario="u329409053_movil";
+$pwd="Guadalupe27";
+$BD="u329409053_movil";
+
+$conn=mysqli_connect($servidor,$usuario,$pwd,$BD)or die("Error al conectarse a la base de datos".mysqli_error());
+
+/*$servidor="localhost";
+$usuario="root";
+$pwd="alex";
+$BD="servcios_libres";*/
+?>
